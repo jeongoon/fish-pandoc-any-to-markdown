11c9a81f27fbd93686c8a1bf4322bd4e25c7a5c8
Myoungjin JEON <jeongoon@gmail.com>
2022-04-19 20:54:21 +1000

    revise before tagging 0.1.1

--- pandoc-any-to-markdown.fish
+++ pandoc-any-to-markdown.fish
@@ -1,6 +1,8 @@
 #!/usr/bin/env fish
 # filename: pandoc-any-to-markdown.fish
 
+set -g pandoc_any_to_markdown_version 0.1.1
+
 set -l input_filepath $argv[1]
 
 if not test -f $input_file
@@ -11,12 +13,19 @@ if not test -f $input_file
     exit 1;
 end
 
+# --| numof_printed
+# --  -1 : metadata not found yet
+# --   0 : metadata entered
+# --   * : metadata is printing
 set -l numof_printed -1
-# ^ indicating phase -1: meta not found (>=0): meta found
-set -l pandoc_from (string split "." $input_filepath | tail -n1)
+
+# --| convert_from
+# --  option which is passed to pandoc with `-f` option
+
+set -l convert_from (string split "." $input_filepath | tail -n1)
 # ^ pandoc reader relys on extension
 
-for x in once
+begin
     while read -l line
         if string match -q --regex '^---' -- "$line"
             set numof_printed (math "$numof_printed + 1")
@@ -32,5 +41,5 @@ for x in once
         end
     end
 
-    pandoc -f $pandoc_from -t markdown -
+    pandoc -f $convert_from -t markdown -
 end < $argv[1]
